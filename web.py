92bbf96c567fb72a9b14567aaa15fe04ffdabd9b
Edevilson <67565328+Edevilson@users.noreply.github.com>
2020-10-13 17:55:13 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Bem vindo ao DevSevOps_Sr Edevilson Mendes!"   "Meu primeiro pipeline"
+    return "Bem vindo ao DevSevOps_"Meu primeiro pipeline"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
